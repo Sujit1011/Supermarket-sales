7c2463b9ae02ac9815fd20735d1ffaba43de5e4a
Sujit Soren <sujitsoren.gms@gmail.com>
2022-09-17 17:43:34 +0530

    Added Application

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Sales Dashboard", 
+        page_icon=":bar_cha:", 
+        layout="wide"
+    )
+
+df = pd.read_excel(io='supermarkt_sales.xlsx',
+        engine='openpyxl',
+        sheet_name='Sales',
+        skiprows=3,
+        usecols='B:R',
+        nrows=1000
+    )
+
+# print(df)
+
+st.dataframe(df)
+
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
